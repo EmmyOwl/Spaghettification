d6f4a0848a47fb26fa5288787c43de52ce674f2e
EmmyOwl <85964661+EmmyOwl@users.noreply.github.com>
2021-06-21 17:57:04 +0200

    config updated, julia working

---
+++ programmierprojekt.py
@@ -0,0 +1,8 @@
+# import pillow
+
+# takes b and h as width and heigth of a scanned png
+# daten is a list of 4-tupels containing the RGBA values of each pixel of the png
+# m is a 3-tupel giving the center point of a 2-sphere in R^3
+# dichte
+# returns a list of 250.000 4-tupls of RGBA values
+#def snapshot_sphere(b,h,daten,m,r,dichte):

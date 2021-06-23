2933749cfaaa3776ff32218e5d349f2904284159
EmmyOwl <85964661+EmmyOwl@users.noreply.github.com>
2021-06-23 23:14:13 +0200

    implemented cases of quadratic equation solutions

--- julia.jl
+++ julia.jl
@@ -1,4 +1,4 @@
-using Base: Int16
+using Base: Int16, Tuple
 using LinearAlgebra
 
 function abbild(p)
@@ -22,11 +22,59 @@ function abbild(p)
     end
 end
     
-#=
-    ψ = lineplanecollision(planenorm, planepnt, pVector, origin)
-    println("Intersection at $ψ")
-=#
+function is_visible(p,m,r)
+    isInImage = abbild(p)
+    numberOfIntersects = intersectSphere(p,m,r)
+    # if p is in the image of the camera and the line segment intersects with the sphere at most in 1 point
+    if  isInImage != nothing && numberOfIntersects == true
+        return true
+    # if p is not in the image of the camera or the line segment intersects with the sphere in a 2nd point
+    else
+        return false
+    end
+end
 
+function intersectSphere(p,m,r)
+    α = p[1]
+    β = p[2]
+    γ = p[3]
+    x0 = m[1]
+    y0 = m[2]
+    z0 = m[3]
+    a = α^2 + β^2 + γ^2
+    b = -2*(α*x0+β*y0+γ*z0)
+    c = (x0^2+y0^2+z0^2-r^2)
+    t = 250/γ
+
+    # catch exeption: if discriminant is negative
+    try
+        D = sqrt(b^2-4*a*c)
+    catch exception
+        if isa(exception, DomainError)
+            solutions = 0
+        end
+    end
+
+    # Case 1: one intersection point
+    if D = 0
+        solutions = 1
+    
+    # Case 2: two intersection points
+    elseif D > 0
+        solutions = 2
+    end
+
+    s_1 = (-b+sqrt(b^2-4*a*c))/(2*a)
+    s_2 = (-b-sqrt(b^2-4*a*c))/(2*a)
+end
+
+####################################### MAIN #################################################
     p = [50,75,100]
     result = abbild(p)
     println(result)
+    m = [50,75,250]
+    r = 50
+    result2 = 0
+    println(result2)
+    result2 = is_visible(p,m,r)
+    #println(result2)

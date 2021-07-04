d402c5cc918906164cbbaa4723d891a0540b6746
Richard <richard@tigerflanke.de>
2021-07-04 21:10:59 +0200

    Adding samples.

--- julia.jl
+++ julia.jl
@@ -29,7 +29,7 @@ end
 
 function is_visible(p, m, r)
     # check whether p lies within camera frame
-    if abbild(p) == nothing
+    if abbild(p) === nothing
         return false
     end
 
@@ -49,8 +49,37 @@ function is_visible(p, m, r)
 end
 
 
-function sample(x, y, b, h, m, r, dichte)
-    return nothing
+function samples(x, y, b, h, m, r, dichte)
+    """Projects a single Pixel onto a specific spot of the sphere and returns an array with the length floor(dichte)^9."""
+    point = spherepoint(x, y, b, h)
+    punkte = [spherpointtranslate(point[1], point[2], m, r)]
+    #Spraying even more points around the original one, according to "dichte".
+    δ = 0 + 1/dichte
+    while δ <= 0.5 && δ > 0
+        deltavals = [(x - δ, x, x + δ), (y - δ, y, y + δ)]
+        for p1 in deltavals[1]
+            for p2 in deltavals[2]
+                if 0 <= p1 <= b && 0 <= p2 <= h
+                    point = spherepoint(p1, p2, b, h)
+                    newpoint = spherpointtranslate(point[1], point[2], m, r)
+                    push!(punkte, newpoint)
+                end
+            end
+        end
+        δ += 1/dichte
+        
+    end 
+    return punkte
+end
+
+function spherepoint(x, y, b, h)
+    """Converts the points into a form suiting for the function.""" 
+    return ((x * pi) / h, (y * 2 * pi) / b)
+end
+
+function spherpointtranslate(x, y, m, r)
+    """Translates the point into its sphere coordinates."""
+    return (m[1] + r * sin(x) * cos(y), m[2] + r * sin(x) * sin(y), m[3] + r * cos(y))
 end
 
 
@@ -69,7 +98,4 @@ function snapshot_sphere(b, h, daten, m, r, dichte)
         end
     end
     return flat
-
-    # flatten and return the array (this flattens the wrong way, I think)
-    #return vcat(bildebene...)
 end
